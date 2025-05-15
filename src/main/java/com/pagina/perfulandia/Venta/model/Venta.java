07c7895351049b05303b1430bf07d42b498792f9
ScarthPz <sca.perezm@duocuc.cl>
2025-05-15 12:30:01 -0400

    correcciones repositorio y servicio

--- src/main/java/com/pagina/perfulandia/Venta/model/Venta.java
+++ src/main/java/com/pagina/perfulandia/Venta/model/Venta.java
@@ -1,5 +1,42 @@
 package com.pagina.perfulandia.Venta.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+import jakarta.persistence.*;
+
+
+
+@Entity
+@Table(name="venta")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Venta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable=false)
+    private Date fecha;
+
+    @Column(nullable=false)
+    private int id_emp;
+
+    @Column(nullable=false)
+    private int id_cli;
+
+    @Column(nullable=false)
+    private int total;
+
+    @Column(nullable=true)
+    private float descuento;
+
+
+
 
 }
