cf50f857b40f05383b956dfe11792dba58b24c20
FernanCastillo <fern.castillop@duocuc.cl>
2025-05-16 22:06:36 -0400

    Se agrega el modelo a Administrador

---
+++ src/main/java/com/pagina/perfulandia/Administrador/model/Administrador.java
@@ -0,0 +1,41 @@
+package com.pagina.perfulandia.Administrador.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Administrador")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Administrador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length=30, nullable=false)
+    private String nombre;
+
+    @Column(length=30, nullable=false)
+    private String ap_paterno;
+
+    @Column(length=30, nullable=false)
+    private String ap_materno;
+
+    @Column(length=80, nullable=false)
+    private String correo;
+
+    @Column(length=80, nullable=false)
+    private String contrasenha;
+
+    @Column(nullable=true)
+    private int num_telefono;
+
+    @Column(nullable=false)
+    private int sueldo;
+
+    @Column(length=80, nullable=false)
+    private int id_sucursal;
+}
